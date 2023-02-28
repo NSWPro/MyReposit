f03a5df13146156bd74279dc59d14bca89231db0
NSWPro <114294502+NSWPro@users.noreply.github.com>
2023-02-28 16:57:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 tg_bot_token = "5779879033:AAFNk5Q5EpzLRbWeqFSI1pCsK7jJfBJz95Q"           # TS_AVIASKh_StatusBot
 #tg_bot_token = "5495336774:AAGMRb0__4YeuRr-4LKoY5PwVTzYl5r37GY"           # NSWTest_Bot
-# send_id_group = -870936808                                              # TS Work
+# send_id_group = -870936808                                              # NSW_Test_Group
 send_id_group = -870936808                                                # TS Work
 send_id_admin = 765159348                                                 # NSW
 dport = 55340
